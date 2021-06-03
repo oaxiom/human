b91ffeeab6f904a24afb9864301ea29953f4604a
oaxiom <oaxiom@gmail.com>
2021-06-03 09:45:49 +0800

    fixed up merges and sample names

--- correlation_maps/corr_map.py
+++ correlation_maps/corr_map.py
@@ -10,9 +10,22 @@ expn = glload("../te_counts/genes_ntc_expression.glb")
 sam_map.remap_expn_sample_names(expn)
 
 expn.log(2, 0.1)
-expn.tree(filename="tree.png", color_threshold=0.0, label_size=4, size=(5,14))
+expn.tree(filename="tree-merged.png", color_threshold=0.0, label_size=4, size=(5,14))
 
-expn.correlation_heatmap(filename="corr_heatmap.png",
+expn.correlation_heatmap(filename="corr_heatmap-merged.png",
+    bracket=(0.0,1.0),
+    size=(14,10),
+    heat_wid=0.57,
+    heat_hei=0.925,
+    row_font_size=4)
+
+expn = glload("../te_counts/genes_ntc_expression_nonmerged.glb")
+sam_map.remap_expn_sample_names(expn)
+
+expn.log(2, 0.1)
+expn.tree(filename="tree-unmerged.png", color_threshold=0.0, label_size=4, size=(5,14))
+
+expn.correlation_heatmap(filename="corr_heatmap-unmerged.png",
     bracket=(0.0,1.0),
     size=(14,10),
     heat_wid=0.57,
